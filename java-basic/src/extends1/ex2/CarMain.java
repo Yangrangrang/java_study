bffc07ccad349b47ab727778338db535bff4979c
yangrangrang <gkssk2309@gmail.com>
2024-11-29 15:47:56 +0900

    #5 - 상속과 메모리구조

--- java-basic/src/extends1/ex2/CarMain.java
+++ java-basic/src/extends1/ex2/CarMain.java
@@ -2,6 +2,11 @@ package extends1.ex2;
 
 public class CarMain {
 
+    /*
+    상속 관계의 객체를 생성하면 그 내부에는 부모와 자식이 모두 생성
+    상속 관계의 객체를 호출 할 때, 대상 타입을 정해야함. 이때 호출자의 타입을 통해 대상 타입을 찾음
+    현재 타입에서 기능을 찾지 못하면 상위 부모 타입으로 기능을 찾아서 실행. 찾지 못하면 컴파일 오류 발생
+     */
     public static void main(String[] args) {
         ElectricCar electricCar = new ElectricCar();
         electricCar.move();
