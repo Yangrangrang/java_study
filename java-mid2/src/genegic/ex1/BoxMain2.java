1f8dafad2c631be044eadce73091342cb78d3bca
yangrangrang <gkssk2309@gmail.com>
2025-01-30 20:34:03 +0900

    #9 - 다형성을 통한 중복 해결 시도

---
+++ java-mid2/src/genegic/ex1/BoxMain2.java
@@ -0,0 +1,21 @@
+package genegic.ex1;
+
+public class BoxMain2 {
+
+    public static void main(String[] args) {
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get();
+        System.out.println("integer = " + integer);
+
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.set("Hello");
+        String string = (String) stringBox.get();
+        System.out.println("string = " + string);
+
+        // 잘못된 타입의 인수 전달시
+        integerBox.set("문제100");
+        Integer result = (Integer) integerBox.get();    // String -> Integer 캐스팅 예외 (ClassCastException)
+        System.out.println("result = " + result);
+    }
+}
