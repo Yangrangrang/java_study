2865902b26035a6029df893c912e4f3119a62baf
yangrangrang <gkssk2309@gmail.com>
2025-02-01 23:07:44 +0900

    #9 - 제네릭 용어와 관례
    - 타입 매개변수, 타입 인자
    - 로타입은 사용하지 않아야 한다.

---
+++ java-mid2/src/genegic/ex1/RowTypeMain.java
@@ -0,0 +1,17 @@
+package genegic.ex1;
+
+public class RowTypeMain {
+
+    /**
+     * <>을 지정하지 않을 수 있는데, 이런것을 로 타입 (row type) 또는 원시 타입이라 한다.
+     * 원시 타입을 사용하면 내부의 타입 매개변수가 Object 로 사용 된다고 이해
+     * Object가 필요하면 GenericBox<Object> integerBox = new GenericBox<>(); 작성하는걸 권장
+     */
+    public static void main(String[] args) {
+        GenericBox integerBox = new GenericBox();
+//        GenericBox<Object> integerBox = new GenericBox<>(); //권장
+        integerBox.set(10);
+        Object result = integerBox.get();
+        System.out.println("result = " + result);
+    }
+}
