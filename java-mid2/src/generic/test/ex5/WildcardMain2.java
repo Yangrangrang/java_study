0b12150b4ca9d9f4f7f1d9ee7865daf063b5a0b1
yangrangrang <gkssk2309@gmail.com>
2025-02-07 19:50:53 +0900

    #9 - 와일드 카드

---
+++ java-mid2/src/generic/test/ex5/WildcardMain2.java
@@ -0,0 +1,31 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildcardMain2 {
+
+    public static void main(String[] args) {
+        Box<Object> objBox = new Box<>();
+        Box<Animal> animalBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        // Animal 포함 상위 타입 전달 가능
+        writeBox(objBox);
+        writeBox(animalBox);
+//        writeBox(dogBox);   // 하한이 Animal
+//        writeBox(catBox);   // 하한이 Animal
+
+        Animal animal = animalBox.get();
+        System.out.println("animal = " + animal);
+    }
+
+    // 하한 지정 (Wildcard에만 적용 할 수 있음)
+    // animal 보다 위에만 들어올 수 있음. <? super Animal>
+    // Animal 보다 하위 타입은 전달 할 수 없음
+    static void writeBox(Box<? super Animal> box) {
+        box.set(new Dog("멍멍", 100));
+    }
+}
