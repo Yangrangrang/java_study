c9009393e625840da9ec7ac36de7a18512fb89d2
yangrangrang <gkssk2309@gmail.com>
2025-02-14 22:09:42 +0900

    #9 - 직접 구현하는 연결 리스트

---
+++ java-mid2/src/collection/link/MyLinkedListV3Main.java
@@ -0,0 +1,22 @@
+package collection.link;
+
+public class MyLinkedListV3Main {
+
+    public static void main(String[] args) {
+        MyLinkedListV3<String> list = new MyLinkedListV3<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        String string = list.get(0);
+        System.out.println("string = " + string);
+
+        MyLinkedListV3<Integer> list2 = new MyLinkedListV3<>();
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+        Integer integer = list2.get(0);
+        System.out.println("integer = " + integer);
+        list2.set(1, 4);
+        System.out.println(list2);
+    }
+}
