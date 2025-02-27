5ec3157a17aeb5ed0c3deb725a39a5462f37df64
yangrangrang <gkssk2309@gmail.com>
2025-02-27 18:45:38 +0900

    #9 - 직접 구현하는 Set4 - 제네릭 인터페이스 도입

---
+++ java-mid2/src/collection/set/MyHashSetV3Main.java
@@ -0,0 +1,22 @@
+package collection.set;
+
+public class MyHashSetV3Main {
+
+    public static void main(String[] args) {
+        MySet<String> set = new MyHashSetV3<>(10);
+        set.add("A");
+        set.add("B");
+        set.add("C");
+        System.out.println("set = " + set);
+
+        // 검색
+        String searchValue = "A";
+        boolean result = set.contains(searchValue);
+        System.out.println("set.contains(" + searchValue + ") = " + result);
+
+        MySet<Integer> set2 = new MyHashSetV3<>(10);
+        set2.add(1);
+        set2.add(2);
+        System.out.println("set2 = " + set2);
+    }
+}
