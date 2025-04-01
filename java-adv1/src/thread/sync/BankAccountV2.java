8772db57a797d476e162e980a052ef250e311d7f
yangrangrang <gkssk2309@gmail.com>
2025-04-01 20:14:55 +0900

    #10 - synchronized 코드 블럭

--- java-adv1/src/thread/sync/BankAccountV2.java
+++ java-adv1/src/thread/sync/BankAccountV2.java
@@ -17,6 +17,7 @@ public class BankAccountV2 implements BankAccount {
     public synchronized boolean withdraw(int amount) {  // 한번에 하나의 쓰레드만 실행할 수 있음.
         log("거래 시작: " + getClass().getSimpleName());
 
+        // == 임계 영역 시작 ==
         // 잔고가 출금액 보다 적으면, 진행하면 안됨
         log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
         if (balance < amount) {
@@ -29,6 +30,7 @@ public class BankAccountV2 implements BankAccount {
         sleep(1000);    // 출금에 걸리는 시간으로 가정
         balance = balance - amount;
         log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        // == 임계 영역 종료 ==
 
         log("거래 종료");
         return true;
