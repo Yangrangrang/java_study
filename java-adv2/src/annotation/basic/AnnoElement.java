58e5b85ebe1088b306b84ea40f6e7aa7bc9aada3
yangrangrang <gkssk2309@gmail.com>
2025-09-14 21:14:40 +0900

    #11 - 메타 애노테이션

--- java-adv2/src/annotation/basic/AnnoElement.java
+++ java-adv2/src/annotation/basic/AnnoElement.java
@@ -2,10 +2,13 @@ package annotation.basic;
 
 import util.MyLogger;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface AnnoElement {
     String value();
     int count() default 0;
