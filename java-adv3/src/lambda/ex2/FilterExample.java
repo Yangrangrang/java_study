9f889071614cdf1956c6a4719bdae34bbc21afe1
yangrangrang <gkssk2309@gmail.com>
2025-10-11 19:24:07 +0900

    #12 - 문제와 풀이2-(1)

---
+++ java-adv3/src/lambda/ex2/FilterExample.java
@@ -0,0 +1,28 @@
+package lambda.ex2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterExample {
+    // 고차 함수, 함수를 인자로 받아서 조건에 맞는 요소만 뽑아내는 filter
+    public static List<Integer> filter(List<Integer> list, MyPredicate predicate) {
+        List<Integer> result = new ArrayList<>();
+        for (int value : list) {
+            if (predicate.test(value)) {
+                result.add(value);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, -2, 3, 0, 5, -10, 7);
+        System.out.println("원본 리스트: " + numbers);
+
+        // 1. 음수(negative)만 뽑아 내기
+
+        // 2. 짝수(even)만 뽑아내기
+        
+    }
+}
