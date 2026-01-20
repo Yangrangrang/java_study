97a746ed36072a170bde53b7a02683a01b197f92
yangrangrang <gkssk2309@gmail.com>
2026-01-20 21:30:17 +0900

    #12 - 필터만들기2

---
+++ java-adv3/src/lambda/lambda5/filter/FilterMain5.java
@@ -0,0 +1,19 @@
+package lambda.lambda5.filter;
+
+import java.util.List;
+
+public class FilterMain5 {
+
+    public static void main(String[] args) {
+        // 숫자 사용 필터
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> evenNumbers = GenericFilter.filter(numbers, n1 -> n1 % 2 == 0);
+        System.out.println("evenNumbers = " + evenNumbers);
+
+        // 문자 사용 필터
+        List<String> strings = List.of("a", "bb", "ccc");
+        List<String> results = GenericFilter.filter(strings, n -> n.length() >= 2);
+        System.out.println("results = " + results);
+    }
+
+}
