994f4f81fb0666e6c2ee87b41c7a17b643206baa
yangrangrang <gkssk2309@gmail.com>
2026-01-29 22:38:43 +0900

    #12 - 맵만들기2 GenericMapper

---
+++ java-adv3/src/lambda/lambda5/map/GenericMapper.java
@@ -0,0 +1,16 @@
+package lambda.lambda5.map;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class GenericMapper {
+
+    public static <T, R> List<R> map(List<T> list, Function<T, R> mapper) {
+        List<R> results = new ArrayList<>();
+        for (T item : list) {
+            results.add(mapper.apply(item));
+        }
+        return results;
+    }
+}
