4b3d3b1a59a1fe6ebd592f3bbea2ef585b2e971f
yangrangrang <gkssk2309@gmail.com>
2026-01-30 23:18:31 +0900

    #12 - 맵만들기2(2)

---
+++ java-adv3/src/lambda/lambda5/map/MapMainV5.java
@@ -0,0 +1,24 @@
+package lambda.lambda5.map;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapMainV5 {
+
+    public static void main(String[] args) {
+        List<String> fruits = Arrays.asList("apple", "banana", "orange", "pineapple");
+
+        // String -> String
+        List<String> upper = GenericMapper.map(fruits, s -> s.toUpperCase());
+        System.out.println("upper = " + upper);
+
+        // String -> Integer
+        List<Integer> length = GenericMapper.map(fruits, s -> s.length());
+        System.out.println("length = " + length);
+
+        // Integer -> String
+        List<Integer> list = List.of(1, 2, 3);
+        List<String> starList = GenericMapper.map(list, n -> "*".repeat(n));
+        System.out.println("starList = " + starList);
+    }
+}
