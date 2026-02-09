dc17cb51f1a1a0979f83d72c04f0f82e59ed0c8d
yangrangrang <gkssk2309@gmail.com>
2026-02-09 22:28:24 +0900

    #12 - 스트림만들기2

---
+++ java-adv3/src/lambda/lambda5/mystream/MyStreamV2Main.java
@@ -0,0 +1,19 @@
+package lambda.lambda5.mystream;
+
+import java.util.List;
+
+public class MyStreamV2Main {
+
+    public static void main(String[] args) {
+        // 짝수만 남기고 남은값의 두배 반환
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        List<Integer> result = MyStreamV2.of(numbers)
+                .filter(number -> number % 2 == 0)
+                .map(number -> number * 2)
+                .toList();
+        
+        System.out.println("result = " + result);
+    }
+
+}
