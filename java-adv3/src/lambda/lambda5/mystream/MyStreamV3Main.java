88701d3e6134086b907ddc117b26f9a02b631a7e
yangrangrang <gkssk2309@gmail.com>
2026-02-12 20:39:34 +0900

    #12 - 스트림만들기3(2)

--- java-adv3/src/lambda/lambda5/mystream/MyStreamV3Main.java
+++ java-adv3/src/lambda/lambda5/mystream/MyStreamV3Main.java
@@ -16,6 +16,18 @@ public class MyStreamV3Main {
         // 점수가 80점 이상인 학생의 이름 추출
         List<String> result1 = ex1(students);
         System.out.println("result1 = " + result1);
+
+        // 점수가 80점 이상이면서, 이름이 5글자인 학생의 이름을 대문자로
+        List<String> result2 = ex2(students);
+        System.out.println("result2 = " + result2);
+    }
+
+    private static List<String> ex2(List<Student> students) {
+        return MyStreamV3.of(students)
+                .filter(s -> (s.getScore() >= 80 && s.getName().length() == 5))
+                .map(s -> s.getName().toUpperCase())
+                .toList();
+
     }
 
     private static List<String> ex1(List<Student> students) {
