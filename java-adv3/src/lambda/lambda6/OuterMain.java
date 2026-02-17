0e08edcd3da7d9fcc947a266d708d72b8c3b6826
yangrangrang <gkssk2309@gmail.com>
2026-02-17 22:33:28 +0900

    #12 -람다 vs 익명클래스1 (2)

--- java-adv3/src/lambda/lambda6/OuterMain.java
+++ java-adv3/src/lambda/lambda6/OuterMain.java
@@ -19,12 +19,25 @@ public class OuterMain {
 
             }
         };
+        // 2. 람다 예시
+        Runnable lambda = () -> {
+            // 람다에서의 this는 람다가 선언되 클래스의 인스턴스(즉, 외부클래스) 가리킴
+            System.out.println("[람다] this" + this);
+            System.out.println("[람다] this.class" + this.getClass());
+            System.out.println("[람다] this.message" + this.message);
+        };
+
+
         anonymous.run();
+        System.out.println("-------------------------------");
+        lambda.run();
     }
 
     public static void main(String[] args) {
         OuterMain outer = new OuterMain();
         System.out.println("[외부클래스] " + outer);
+        System.out.println("-------------------------------");
+        
         outer.execute();
     }
 }
